76c1f87a3ede2cfac3a0ab0a0c037859ce25188e
uttamtamang <uttamtamang45@gmail.com>
2019-11-11 12:41:22 +0545

    comit

--- app/src/main/java/com/example/usa_presidents/SpinnerDisplayActivity.java
+++ app/src/main/java/com/example/usa_presidents/SpinnerDisplayActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SpinnerDisplayActivity extends AppCompatActivity {
 
@@ -13,5 +14,24 @@ public class SpinnerDisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner_display);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        tvName = findViewById(R.id.yName);
+        tvPhone= findViewById(R.id.pNumber);
+        tvEmail = findViewById(R.id.email);
+        tvAddress = findViewById(R.id.address);
+
+        Bundle bundleForm = getIntent().getExtras();
+        if(bundleForm != null){
+            tvName.setText(bundleForm.getString("forName"));
+            tvPhone.setText(bundleForm.getString("forPhone"));
+            tvEmail.setText(bundleForm.getString("forEmail"));
+            tvAddress.setText(bundleForm.getString("forAddress"));
+
+        }
+        else{
+            Toast.makeText(this, "Sorry, no data found.", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
